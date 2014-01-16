0cb508ae90d1b6d3dea9e31fedd8e2ebbc4983b4
Rob Barrett <rob.barrett@ventyx.abb.com>
2014-01-16 13:27:52 +1000

    spawn new process to iterate over constructed classpath

--- JavaServiceComparison/src/foo/JavaServiceComparison.java
+++ JavaServiceComparison/src/foo/JavaServiceComparison.java
@@ -1,19 +1,17 @@
 package foo;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JavaServiceComparison {
 
-	private final File from;
-	private final File to;
-
 	public static void main(String[] args) {
-
 		if (args.length < 2) {
 			throw new IllegalArgumentException("expected 2 arguments");
 		}
-
 		String fromPath = args[0];
 		File from = new File(fromPath);
 		if (!from.exists()) {
@@ -31,21 +29,48 @@ public class JavaServiceComparison {
 		if (!to.isDirectory()) {
 			throw new IllegalArgumentException(toPath + " must be a directory");
 		}
-
-		new JavaServiceComparison(from, to).run();
-
+		new JavaServiceComparison(from, to);
 	}
 
 	public JavaServiceComparison(File from, File to) {
-		this.from = from;
-		this.to = to;
 		List<String> fromJars = new EARJarLister(from).list();
-		System.out.println(fromJars);
+		List<String> fromExtracted = runExtraction(fromJars);
+		for (String s : fromExtracted) {			
+			System.out.println(s);
+		}
 		List<String> toJars = new EARJarLister(to).list();
-		System.out.println(toJars);
+		List<String> toExtracted = runExtraction(toJars);
+		for (String s : fromExtracted) {			
+			System.out.println(s);
+		}
 	}
 
-	private void run() {
+	private List<String> runExtraction(List<String> jars) {
+		StringBuilder sb = new StringBuilder();
+		for (String s : jars) {
+			sb.append(s).append(":");
+		}
+		sb.append(ServiceFromClasspathExtractor.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+		String jvm = new File(new java.io.File(
+				System.getProperty("java.home"), "bin"), "java")
+				.getAbsolutePath();
+		ProcessBuilder pb = new ProcessBuilder(jvm, "-classpath", sb.toString(), ServiceFromClasspathExtractor.class.getName());
+		pb.inheritIO();
+		try {
+			File temp = File.createTempFile("foo", ".txt");
+			pb.redirectOutput(temp);
+			Process p = pb.start();
+			p.waitFor();
+			BufferedReader br = new BufferedReader(new FileReader(temp));
+			List<String> results = new ArrayList<String>();
+			String line;
+			while ((line = br.readLine()) != null) {
+				results.add(line);
+			}
+			br.close();
+			return results;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
-
 }
---
+++ JavaServiceComparison/src/foo/ServiceFromClasspathExtractor.java
@@ -0,0 +1,9 @@
+package foo;
+
+public class ServiceFromClasspathExtractor {
+	
+	public static void main(String[] args) {
+		System.out.println("extracting services from classpath");
+	}
+
+}
